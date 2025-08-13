f891057dd068c286767aafa35da5e39f80479d1a
Dylan <dylsimons@gmail.com>
2025-08-12 21:37:05 -0400

    back to html

--- Website_control/facetracking/views.py
+++ Website_control/facetracking/views.py
@@ -1,12 +1,6 @@
-from django.http import HttpResponse
+from django.shortcuts import render
 
+# Create your views here.
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
-
-# from django.shortcuts import render
-
-# # Create your views here.
-
-# def index(request):
-#     return render(request, "facetracking/index.html", {})
+    return render(request, "facetracking/index.html", {})
